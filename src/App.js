ed4b9d9ef78c80219a3b89d5f8837f9a3ca25deb
Santiagojbp <86267406+Santiagojbp@users.noreply.github.com>
2021-12-13 23:38:55 -0300

    componentes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './sass/App.scss';
+import NavComponents from './components/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavComponents/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from './../assets/img/fondos/icono2.0.jpg';
+export default NavComponents 
+function NavComponents() {
+	return (
+		<div>
+			<div className="contenedor-img">
+				<img src={logo} alt="logo" className="App-logo"/>
+			</div>
+			<div>
+				<nav>
+					<ul className="contenedor-li">
+						<li className="nav-li"><a href="">Inicio</a></li>
+						<li className="nav-li">
+							<a >Categorias<i className="bi bi-caret-down"></i></a>
+							<ul className="contenedor-categorias">
+								<li className="li-categorias"><a href="">limpieza</a></li>
+								<li className="li-categorias"><a href="">limpieza</a></li>
+								<li className="li-categorias"><a href="">limpieza</a></li>
+								<li className="li-categorias"><a href="">limpieza</a></li>
+								<li className="li-categorias"><a href="">limpieza</a></li>
+							</ul>
+						</li>
+						<li className="nav-li"><a href="">Crear cuenta</a></li>
+						<li className="nav-li"><a href="">Ayuda</a></li>
+						<li className="nav-li"><a href="">Quienes somos</a></li>
+					</ul>
+				</nav>
+			</div>	
+		</div> )
+}
