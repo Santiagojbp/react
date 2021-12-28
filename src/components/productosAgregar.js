f7b445b4564a8448c5340be6ec6489e84ff26857
Santiagojbp <86267406+Santiagojbp@users.noreply.github.com>
2021-12-27 23:33:34 -0300

    cambios

--- src/components/productosAgregar.js
+++ src/components/productosAgregar.js
@@ -1,25 +1,28 @@
 import inventario from './../data/inventario';
+export default AgregarProductos
 function AgregarProductos() {
-  return(
+return(
     <>  
       {inventario.map( producto=>{
               return(
-                <div class="col-lg-2 contenedor-card" id="producto">
-                  <div class="contenedorCard">
-                      <div class="card img">
-                        <img class="card__img img-card" src="{producto.imagen} " alt="Imagen de {producto.nombre} "/>
-                        <div class="card__text producto-datos">
-                            <input value="{producto.id}" type="hidden"/>
-                            <h3 class="card__title producto-nombre">{producto.nombre}</h3>
-                            <p class="precio">"$"{producto.precio}</p>
+                <div className="col-lg-2 contenedor-card" id="producto">
+                  <div className="contenedorCard">
+                    <div className="card img">
+                      <img className="card__img img-card" src="{producto.imagen} " alt="Imagen de {producto.nombre} "/>
+                      <div className="card__text producto-datos">
+                        <input value="{producto.id}" type="hidden"/>
+                        <h3 className="card__title producto-nombre">{producto.nombre}</h3>
+                        <p className="precio">"$"{producto.precio}</p>
                         <div>
-                            <button id="boton{producto.id}" class="agregar-carrito" style="margin-bottom:3px;margin-left:3px">agregar<i class="bi bi-cart-plus"></i></button>
+                            <button id="boton{producto.id}" className="agregar-carrito" >agregar<i className="bi bi-cart-plus"></i></button>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
+        )}
       )}
   </>
 )
 }
+
